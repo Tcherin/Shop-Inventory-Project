4a4b865fdc683adb9d97f4ef4d32a295c2b45ee4
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2023-07-15 11:54:56 +0100

    fixed multiple stylsheets

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.register_blueprint(suppliers_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('landing.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
